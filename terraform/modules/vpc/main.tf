7d78a13df0364aa510a6f9c2c742c45d99f34e6e
Emanuel Ernesto González Michea <emanuelgm1998@users.noreply.github.com>
2026-06-26 07:29:55 -0400

    feat(vpc): add private subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -12,20 +12,6 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_internet_gateway" "this" {
- resource "aws_vpc" "this" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "${var.project_name}-${var.environment}-vpc"
-    }
-  )
-}
-
-resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
   tags = merge(
