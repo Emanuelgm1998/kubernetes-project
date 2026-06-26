953fbe0ec37cd476cab3a048179efd7f72896625
Emanuel Ernesto González Michea <emanuelgm1998@users.noreply.github.com>
2026-06-26 07:07:04 -0400

    feat(vpc): add public subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -21,3 +21,21 @@ resource "aws_internet_gateway" "this" {
     }
   )
 }
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
+      Type = "Public"
+    }
+  )
+}
+
