3f66b4bf2c6c157c92ce7bfdce5775cad6ce1b44
Emanuel Ernesto González Michea <emanuelgm1998@users.noreply.github.com>
2026-06-26 07:14:02 -0400

    feat(vpc): add private subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -12,6 +12,20 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_internet_gateway" "this" {
+ resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-vpc"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
   tags = merge(
@@ -35,7 +49,26 @@ resource "aws_subnet" "public" {
     {
       Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
       Type = "Public"
+
+      "kubernetes.io/role/elb" = "1"
     }
   )
 }
 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-private-${count.index + 1}"
+      Type = "Private"
+
+      "kubernetes.io/role/internal-elb" = "1"
+    }
+  )
+}
